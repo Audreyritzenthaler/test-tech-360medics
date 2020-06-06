5f611070c483384fcfb0d3376d5bc8ab15fbeef2
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-06-06 13:36:47 +0200

    get router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';;
+import React from 'react';
 import Liste from './components/Liste'
+import ProductSheet from './components/ProductSheet';
+import { Switch, Route } from "react-router-dom"
 
 import './App.css';
 
 function App() {
+
+
   return (
     <div className="App">
-      <Liste />
+    <Switch>
+      <Route exact path="/" component={Liste}/>
+      <Route path="/product/:product_name" component={ProductSheet}/>
+    </Switch>
     </div>
   );
 }
